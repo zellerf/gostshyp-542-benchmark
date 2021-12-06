cc5bb72ba8078523702ee4ad200987d794d6dbdd
zellerf <zellerf@uni-bremen.de>
2021-12-06 12:28:05 +0100

    added function to parse gradient timings

--- get_timings.py
+++ get_timings.py
@@ -1,9 +1,9 @@
 import sys
 
+
 # parses file fo scf times in CPU seconds scaled with number of corresponding SCF cycles
 # filename[in]: string, name of file to parse
 # timings[return] list of CPUs /ncycles
-
 def parse_scf_timings(filename):
     scf_times, scf_cylces = [], []
     # counters for number of input files detected and for finished calcs, only for error detection
@@ -54,9 +54,47 @@ def parse_scf_timings(filename):
     # calc SCF time per SCF cycle for each SCF
     timings = []
     for i in range(len(scf_times)):
-        timings.append(scf_times[i]/scf_cylces[i])
+        timings.append(scf_times[i] / scf_cylces[i])
 
     return timings
+
+
+def parse_grad_times(filename):
+    grad_times = []
+    # counters for number of input files detected and for finished calcs, only for error detection
+    input_detected, calc_finished = 0, 0
+
+    try:
+        with open(filename) as file:
+            lines = file.readlines()
+            for line in lines:
+                # check if file only contains ONE finished calculation
+                if 'User input:' in line:
+                    input_detected += 1
+                if 'Thank you very much for using Q-Chem.  Have a nice day.' in line:
+                    calc_finished += 1
+                # get grad time
+                if ' Gradient time:' in line:
+                    try:
+                        splitline = line.split()
+                        grad_times.append(float(splitline[3].strip('s')))
+                    except IOError:
+                        print('Error while reading SCF times in ' + filename)
+                        exit(1)
+
+    except IOError:
+        print('cannot open ' + filename)
+        exit(1)
+
+    # crash if file did not contain only one calc or was crashed
+    if input_detected != 1:
+        print('Error: multiple calcs detected in ' + filename)
+        exit(1)
+    if calc_finished != 1:
+        print('Error: crashed calculation in ' + filename)
+        exit(1)
+
+    return grad_times
 
 
 def main():
@@ -64,11 +102,17 @@ def main():
 
     # get SCF times
     scf_timings = parse_scf_timings(filename)
-    print('SCF times /ncycles:')
+    print('SCF times /ncycles [CPUs]:')
     for i in scf_timings:
         print(i)
 
+    # get Grad times
+    grad_timings = parse_grad_times(filename)
+    print('Gradient times [CPUs]')
+    for i in grad_timings:
+        print(i)
     return 0
+
 
 if __name__ == '__main__':
     exit(main())
