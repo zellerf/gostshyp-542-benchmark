29a615bf4d700d54e0459dcbcedd49edfb2dad28
zellerf <zellerf@uni-bremen.de>
2021-12-08 18:18:47 +0100

    added plotfunction for energy

---
+++ plots.py
@@ -0,0 +1,35 @@
+import sys
+import matplotlib.pyplot as plt
+
+def plot_energy(out, ref):
+    # check input parameters for correct data type
+    if not isinstance(out, dict):
+        print('passed wrong argument to function plot_energy')
+        sys.exit(1)
+    if not isinstance(ref, dict):
+        print('passed wrong argument to function plot_energy')
+        sys.exit(1)
+
+    # read data from dicts
+    nbsf, energies = [], []
+    for key in out.keys():
+        try:
+            nbsf.append(int(out[key]['nbsf']))
+            # energy difference between out and reference
+            energies.append(abs(float(out[key]['energy'] - ref[key]['energy'])))
+        except (KeyError, ValueError, TypeError):
+            print('Error while digesting energies')
+            sys.exit(1)
+
+    fig = plt.figure()
+    ediff = plt.subplot()
+    scf_conv = plt.subplot()
+    # add dashed line for SCF convergence thresh
+    scf_thresh = [1e-8]*len(nbsf)
+    scf_conv.plot(nbsf, scf_thresh, linestyle='dashed', marker='')
+    ediff.scatter(nbsf, energies, label='$\Delta E$', marker='x')
+    scf_conv.set_yscale('log')
+    ediff.set_yscale('log')
+    plt.xlabel("number of basis functions")
+    plt.ylabel("$\Delta E$ [Hartree]")
+    fig.savefig("pes.pdf")
